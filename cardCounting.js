7289c5ebe9f15bfc7bc95064f01455aed83acaf1
Anandhu Vijayan <38665597+Anandhuvijayan@users.noreply.github.com>
2021-02-05 12:15:43 +0100

    Update cardCounting.js

--- cardCounting.js
+++ cardCounting.js
@@ -1,6 +1,13 @@
+/* You will write a card counting function. It will receive a card 
+parameter, which can be a number or a string, and increment or decrement 
+the global count variable according to the card's value (see table). 
+The function will then return a string with the current count and the 
+string Bet if the count is positive, or Hold if the count is zero or negative. 
+The current count and the player's decision (Bet or Hold) should be separated by a single space.*/
+
 var count = 0;
 function cc(card) {
-  // Only change code below this line
+  
   var regex = /[JQKA]/;
   if (card > 1 && card < 7) {
     count++;
@@ -11,7 +18,7 @@ function cc(card) {
   if (count > 0) return count + " Bet";
   return count + " Hold";
 
-  // Only change code above this line
+ 
 }
 
 cc(2);
