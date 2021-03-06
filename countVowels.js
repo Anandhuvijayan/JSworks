b819e97a9e7b850698c844c6626f2f3fd853af4d
Anandhu Vijayan <38665597+Anandhuvijayan@users.noreply.github.com>
2021-03-06 20:31:58 +0100

    Update countVowels.js

--- countVowels.js
+++ countVowels.js
@@ -4,11 +4,9 @@ function countVowel(str) {
   // find the count of vowels
   const count = str.match(/[aeiou]/gi).length;
 
-  // return number of vowels
   return count;
 }
 
-// take input
 const string = prompt("Enter a string: ");
 
 const result = countVowel(string);
